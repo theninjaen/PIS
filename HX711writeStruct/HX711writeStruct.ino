b2ba44419c13fcf9e9164ab472499c5d06204ba4
Martin <martinporthouse@hotmail.com>
2024-12-05 10:51:09 +0100

    heart rate test

--- HX711writeStruct/HX711writeStruct.ino
+++ HX711writeStruct/HX711writeStruct.ino
@@ -1,7 +1,9 @@
 #include "HX711.h" //This library can be obtained here http://librarymanager/All#Avia_HX711
 
-#define LOADCELL_DOUT_PIN  2
+#define LOADCELL_DOUT_PIN  4
 #define LOADCELL_SCK_PIN  3
+
+int HEART_RATE_PIN = 2;
 
 HX711 scale;
 
@@ -13,17 +15,21 @@ void setup() {
   scale.set_scale();
   scale.tare();  // Reset the scale to 0
   scale.set_scale(calibration_factor); // Adjust to this calibration factor
+
+  pinMode(HEART_RATE_PIN, INPUT);
 }
 
 void loop() {
   float weight = scale.get_units();  // Get the weight reading
-
+  int beat = digitalRead(HEART_RATE_PIN);
   // // Print the weight as a readable number
   // Serial.print("Weight: ");
   // Serial.print(weight);
   // Serial.println(" kg");  // Assuming the weight is in kilograms
 
   Serial.print(scale.get_units(), 1);
+  Serial.println();
+  Serial.print(beat);
   Serial.println();
   
   delay(1000/30);  // Add a delay to control the rate of data sending
