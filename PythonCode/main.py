364a43c883d988cf438af8fa0bd42f530a19e0cf
Martin <martinporthouse@hotmail.com>
2024-12-09 15:29:07 +0100

    Heart Rate

--- PythonCode/main.py
+++ PythonCode/main.py
@@ -1,6 +1,6 @@
 from setup import create_segment, setup_game_window, setup_pen
 from keyboard_control import setup_keyboard_reader
-from serial_connection import setup_serial_connection, create_arduino_thread, read_weight_adjusted_interval, MAX_UPDATE_INTERVAL
+from serial_connection import setup_serial_connection, create_arduino_thread, read_weight_adjusted_interval, read_heart_rate, MAX_UPDATE_INTERVAL
 from game_logic import move, check_death_collisions, check_food_collision, enemy_decide_direction
 import turtle
 import time
@@ -43,14 +43,12 @@ def game_loop():
     player_update_timer += time_delta
     enemy_update_timer += time_delta
 
-    print(read_weight_adjusted_interval())
-
     if player_update_timer >= read_weight_adjusted_interval():
         check_food_collision(snake_head, snake_body, enemy_head, enemy_body, food, pen)
         move(snake_head, snake_body)
         player_update_timer = 0
     
-    if enemy_update_timer >= enemy_update_interval:
+    if enemy_update_timer >= read_heart_rate():
         enemy_head.direction = enemy_decide_direction(enemy_head)
         move(enemy_head, enemy_body)
         enemy_update_timer = 0
